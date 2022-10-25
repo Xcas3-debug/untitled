82b06c46480f6cacd15311c48e958998487eae96
Павел Урасов <yrasov.pavel@mail.ru>
2022-10-25 18:41:11 +0500

    lb2

---
+++ src/lr2/example21_03.java
@@ -0,0 +1,21 @@
+package lr2;
+import java.util.Scanner;
+public class example21_03 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Число:");
+        int a = in.nextInt();
+        if (a%4 == 0) {
+            System.out.println("Делится на 4 без остатка");
+        }
+        else {
+            System.out.println("Не делится на 4 без остатка");
+        }
+        if (a > 10) {
+            System.out.println("Число больше 10");
+        }
+        else {
+            System.out.println("Число меньше или равно 10");
+        }
+    }
+    }
