84fca14afb168b4c7d2a93507311fdd02417d3d3
Павел Урасов <yrasov.pavel@mail.ru>
2022-10-26 15:23:38 +0500

    lb2

--- src/lr3/lr3example15_04.java
+++ src/lr3/lr3example15_04.java
@@ -1,6 +1,26 @@
 package lr3;
-
+import java.util.Scanner;
 public class lr3example15_04 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите 2 целых числа: ");
+        int numOne = in.nextInt();
+        int numTwo = in.nextInt();
 
+        System.out.println("Решение через for: ");
+        if (numOne < numTwo)
+            for (int i = numOne; i <= numTwo; i++) System.out.print(i + " ");
+        else if (numOne > numTwo) {
+            for (int i = numTwo; i <= numOne; i++) System.out.print(i + " ");
+        } else System.out.println(numTwo);
 
+        System.out.println("\nРешение через while: ");
+        int abs = Math.abs(numOne - numTwo);
+        int lower = Math.min(numOne, numTwo);
+        while (abs-- >= 0) {
+            System.out.print(lower + " ");
+            lower++;
+        }
+
+    }
 }
